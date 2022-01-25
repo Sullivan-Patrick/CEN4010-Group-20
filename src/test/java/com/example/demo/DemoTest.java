7f8e55d8785670589b0071367d03fcf955f577d8
Patrick Sullivzn <patrick@Patricks-Air.cfl.rr.com>
2022-01-24 21:17:04 -0500

    cleaned up pom and removed explicit surefire stuff

--- src/test/java/com/example/demo/DemoTest.java
+++ src/test/java/com/example/demo/DemoTest.java
@@ -4,25 +4,23 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class DemoTest {
 
   Demo demo = new Demo();
 
-  @Before
+  @BeforeEach
   public void setUp() {
     System.out.println("Tests beginning");
   }
 
   @Test
   public void shouldBeADemo() {
-    for(int i = 0; i < 10000; i++)
-    System.out.println("Test entered");
     assertThat(demo, is(instanceOf(Demo.class)));
   }
 }
