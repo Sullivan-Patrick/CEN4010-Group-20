b01fb93eb272700c34430063866feca27383d6db
danisua2610 <97620093+danisua2610@users.noreply.github.com>
2022-03-21 18:12:57 -0400

    Update BookRatingController.java

--- src/main/java/com/cen4010/bookstore/bookRating/BookRatingController.java
+++ src/main/java/com/cen4010/bookstore/bookRating/BookRatingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
     @RestController
-    @RequestMapping("/api/book")
+    @RequestMapping("/api/bookRating")
     public class BookRatingController {
 
         private final BookRatingService bookRatingService;
