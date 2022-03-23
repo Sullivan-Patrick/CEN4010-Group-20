e14f3463b6b269bbfa008e57cb805735309c944c
Patrick Sullivzn <patrick.sullivan1997@gmail.com>
2022-03-23 15:28:41 -0400

    initial

---
+++ src/test/java/com/cen4010/bookstore/NobodyBreaksCompilationTest.java
@@ -0,0 +1,12 @@
+package com.cen4010.bookstore;
+
+import org.junit.jupiter.api.Test;
+
+public class NobodyBreaksCompilationTest {
+
+  @Test
+  public void nobodyBreaksCompilation() {
+    BookStoreRunner.main(new String[0]);
+  }
+
+}
